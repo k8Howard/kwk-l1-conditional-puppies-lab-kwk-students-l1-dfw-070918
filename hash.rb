ebf9c4a7b5256fa263e43533c58ffccf43d45f4c
k8Howard <sweetk8@me.com>
2018-07-11 18:36:06 +0000

    automatically backed up by learn

--- hash.rb
+++ hash.rb
@@ -1,4 +1,4 @@
-shopping_hash = {"24K magic CD" => 15.98, "crocs" => 35, "potato" =}
+shopping_hash = {"24K magic CD" => 15.98, "crocs" => 35, "potato" => 0.8, "ACT prep book" => 15}
 
 shopping_hash.each do |item_name, price|
   puts "item:#{item_name}"
